ff82723ba4eeed214c0722784f1d0f7bca061734
Glider <g4regtrash@gmail.com>
2018-08-06 00:22:30 +0300

    working char list with char links (still from db, api not connected yet)

--- advisor/chars.py
+++ advisor/chars.py
@@ -5,16 +5,25 @@ from flask import Flask
 
 from .db_connector import get_db
 
+from .gw2apiwrapper import ApiWrapper
+
 char_blueprint = Blueprint('chars', __name__, url_prefix = '/char')
 
 
 @char_blueprint.route('/list', methods = ('GET', ))
 def char_list():
     if request.method == 'GET':
-        db = get_db
+        db = get_db()
         error = None
-        return render_template('char.html', content = "char list")
+        #api_wrapper = ApiWrapper(g.api_key)
+        #chars_json = api_wrapper.get_json_string('characters', None)
 
+        key_id = db.execute(f"SELECT id from api_key WHERE key_value = '{g.api_key}'").fetchone()
+
+        chars = db.execute(f"SELECT id from character WHERE api_id = '{key_id[0]}'").fetchall()
+        char_names = [char[0] for char in chars]
+
+        return render_template('characters.html', char_names = char_names)
 
 
 @char_blueprint.route('/<char_name>', methods = ('GET', ))
@@ -23,12 +32,13 @@ def char(char_name):
         db = get_db()
         error = None
 
-        char_row = db.execute(f"SELECT json_string FROM `character` WHERE id = '{char_name}'").fetchone()
+        char_row = db.execute(f"SELECT json_string, last_update FROM `character` WHERE id = '{char_name}'").fetchone()
         char_json = char_row[0]
+        creation_time = char_row[1]
 
 
         if char_json is None:
             error = 'Character not found'
 
         flash(error)
-        return render_template('char.html', content = char_json)
+        return render_template('char.html', content = char_json, timestamp = creation_time)
